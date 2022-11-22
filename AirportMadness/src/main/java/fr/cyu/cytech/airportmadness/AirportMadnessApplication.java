ba5433e08b94d7fb2accb4be9c51a5b87f27d7a5
ShaitanLyss <33299808+ShaitanLyss@users.noreply.github.com>
2022-11-22 13:12:01 +0100

    Add spring project

---
+++ AirportMadness/src/main/java/fr/cyu/cytech/airportmadness/AirportMadnessApplication.java
@@ -0,0 +1,13 @@
+package fr.cyu.cytech.airportmadness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportMadnessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirportMadnessApplication.class, args);
+    }
+
+}
---
+++ AirportMadness/src/test/java/fr/cyu/cytech/airportmadness/AirportMadnessApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cyu.cytech.airportmadness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportMadnessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
