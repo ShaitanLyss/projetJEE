79d19b6c354bf12d5e1082c68e947ca3c3779657
rodriguez001 <kamgaingrodrigue3@gmail.com>
2022-11-23 17:21:31 +0100

    Create Entities
    airport, employee, Country, city et les repositories

---
+++ AirportMadness/src/main/java/fr/cyu/airportmadness/entity/country/CountryRepository.java
@@ -0,0 +1,6 @@
+package fr.cyu.airportmadness.entity.country;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CountryRepository extends PagingAndSortingRepository<Country, Long> {
+}
---
+++ AirportMadness/src/main/java/fr/cyu/airportmadness/entity/person/employee/Employee.java
@@ -0,0 +1,12 @@
+package fr.cyu.airportmadness.entity.person.employee;
+
+import fr.cyu.airportmadness.entity.person.Person;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table()
+public class Employee extends Person {
+
+}
