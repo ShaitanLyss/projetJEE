7df586100af31e99f5316861ff7eb65e96b3e402
Rodrigue k <kamgaingrodrigue3@gmail.com>
2022-12-07 22:48:22 +0100

    Deleting of PebbleEngineFactory file

---
+++ AirportMadness/src/main/java/fr/cyu/airportmadness/security/PebbleEngineFactory.java
@@ -0,0 +1,21 @@
+package fr.cyu.airportmadness.security;
+
+import io.pebbletemplates.pebble.PebbleEngine;
+import io.pebbletemplates.pebble.loader.ClasspathLoader;
+import io.pebbletemplates.pebble.loader.Loader;
+
+public class PebbleEngineFactory {
+
+    /**
+     * Generates a pebble engine with our extensions
+     * and the "templates" path prefix.
+     */
+    private static Loader loader = new ClasspathLoader();
+    private  static PebbleEngine.Builder pebbleEngine = new PebbleEngine.Builder();
+    public static PebbleEngine buildPebbleEngine() {
+        loader.setPrefix("templates/pebble");
+        loader.setSuffix(".peb");
+        return pebbleEngine.loader(loader).build();
+    }
+}
+
